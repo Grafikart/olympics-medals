db89e69a630bb75495884b7dec5262f764e048f5
jonathan <contact@grafikart.fr>
2024-08-03 23:39:43 +0200

    ref: remove useless elements

--- src/main.tsx
+++ src/main.tsx
@@ -1,63 +1,81 @@
-import {render} from "preact";
+import { render } from "preact";
 import "./reset.css";
 import "./main.css";
 import continents from "./continents.json";
-import {useIncrementalNumber} from "./hooks/useIncrementalNumber.ts";
+import { useIncrementalNumber } from "./hooks/useIncrementalNumber.ts";
 
 type ItemOf<T> = T extends (infer U)[] ? U : never;
-type Continent = ItemOf<typeof continents>
-
+type Continent = ItemOf<typeof continents>;
 
 /**
  * Rings representing the number of medals of continents
  */
 function Graph() {
-    const totals = continents.map(c => c.total)
-    return <div className="continents">
-        {continents.map((continent, k) => (
-            <Ring {...continent} key={continent.name} index={k} min={Math.min(...totals)} max={Math.max(...totals)}/>))}
-    </div>;
+  const totals = continents.map((c) => c.total);
+  return (
+    <div className="continents">
+      {continents.map((continent, k) => (
+        <Ring
+          {...continent}
+          key={continent.name}
+          index={k}
+          min={Math.min(...totals)}
+          max={Math.max(...totals)}
+        />
+      ))}
+    </div>
+  );
 }
 
 /**
  * Ring for a continent
  */
-function Ring({name, gold, silver, bronze, total, index, min, max}: Continent & {
-    index: number,
-    min: number,
-    max: number
+function Ring({
+  name,
+  gold,
+  silver,
+  bronze,
+  total,
+  index,
+  min,
+  max,
+}: Continent & {
+  index: number;
+  min: number;
+  max: number;
 }) {
-    const ratio = (total - min) / (max - min)
-    const style = {
-        gridColumnStart: Math.floor(index / 2) + 1,
-        '--index': index,
-        '--ratio': ratio,
-        '--color': `var(--ring${index + 1})`
-    }
-    const count = useIncrementalNumber(total, 700, 1000)
-    return <article className="continent" style={style}>
-        <header>
-            <h2>{name}: <strong>{count}</strong></h2>
-            <ul className="continent__count">
-                <Medal type="Gold" count={gold}/>
-                <Medal type="Silver" count={silver}/>
-                <Medal type="Bronze" count={bronze}/>
-            </ul>
-        </header>
-        <div className="ring">
-            <div className="ring__l"></div>
-            <div className="ring__r"></div>
-            <div className="ring__b"></div>
-            <div className="ring__t"></div>
-        </div>
+  const ratio = (total - min) / (max - min);
+  const style = {
+    gridColumnStart: Math.floor(index / 2) + 1,
+    "--index": index,
+    "--ratio": ratio,
+    "--color": `var(--ring${index + 1})`,
+  };
+  const count = useIncrementalNumber(total, 700, 1000);
+  return (
+    <article className="continent" style={style}>
+      <header>
+        <h2>
+          {name}: <strong>{count}</strong>
+        </h2>
+        <ul className="continent__count">
+          <Medal type="Gold" count={gold} />
+          <Medal type="Silver" count={silver} />
+          <Medal type="Bronze" count={bronze} />
+        </ul>
+      </header>
+      <div className="ring"></div>
     </article>
+  );
 }
 
-function Medal({type, count}: { type: string, count: number }) {
-    const animatedCount = useIncrementalNumber(count, 700, 1000)
-    return <li className={type.toLowerCase()} aria-label={`${type} medals`}>
-        {animatedCount}
+function Medal({ type, count }: { type: string; count: number }) {
+  const animatedCount = useIncrementalNumber(count, 700, 1000);
+  return (
+    <li className={type.toLowerCase()} aria-label={`${type} medals`}>
+      {animatedCount}
     </li>
+  );
 }
 
-render(<Graph/>, document.getElementById("graph")!);
+render(<Graph />, document.getElementById("graph")!);
